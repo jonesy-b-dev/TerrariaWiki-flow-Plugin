8e188da9b62a7a81444f7f7d59c28c2b5da7bc2b
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-01-22 16:11:57 +0100

    Initial functionality for search on the terraria wiki

--- TerrariaWiki/Flow.Launcher.Plugin.TerrariaWiki/Main.cs
+++ TerrariaWiki/Flow.Launcher.Plugin.TerrariaWiki/Main.cs
@@ -1,6 +1,10 @@
 using Flow.Launcher.Plugin;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Policy;
+using Flow.Launcher.Plugin.SharedCommands;
 
 namespace Flow.Launcher.Plugin.TerrariaWiki
 {
@@ -8,16 +12,35 @@ namespace Flow.Launcher.Plugin.TerrariaWiki
     {
         private PluginInitContext _context;
 
-        private readonly string base_url = "https://en.cppreference.com";
+        private readonly string base_url = "https://terraria.fandom.com/";
+        private string query_url;
+
+        private readonly string icon_path = "icon.png";
 
         public void Init(PluginInitContext context)
         {
+            query_url = base_url + "api.php?action=query&list=search&srwhat=text&format=json&srsearch=";
+
             _context = context;
         }
 
         public List<Result> Query(Query query)
         {
-            return new List<Result>();
+
+            string finalUrl = base_url + "wiki/" + query.Search;
+
+            var result = new Result
+            {
+                Title = "`Search Terraria wiki",
+                SubTitle = $" {query_url+  query.Search}",
+                Action = e =>
+                {
+                    finalUrl.OpenInBrowserTab();
+                    return true;
+                },
+                IcoPath = "icon.png"
+            };
+            return new List<Result>() { result };
         }
     }
 }
