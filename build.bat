a7cf52d5bd7a8a6592ed6e106c8299e9ff66f91b
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-01-23 14:38:51 +0100

    Reset build file again

--- build.bat
+++ build.bat
@@ -2,12 +2,8 @@
 
 taskkill /im Flow.Launcher.exe
 
-set "sourceFolder=D:\Programming\TerrariaWiki-flow-Plugin\TerrariaWiki\Flow.Launcher.Plugin.TerrariaWiki\bin\Debug"
-set "destinationFolder=C:\Users\xvanhunen\AppData\Roaming\FlowLauncher\Plugins\TerrariaWiki"
-
-echo Removing plugin folder...
-
-rd /s /q "%destinationFolder%"
+set "sourceFolder=D:\Code Projects\C#\TerrariaWiki-flow-Plugin\TerrariaWiki\Flow.Launcher.Plugin.TerrariaWiki\bin\Debug"
+set "destinationFolder=C:\Users\jonas\AppData\Local\FlowLauncher\app-1.16.2\UserData\Plugins\TerrariaWiki"
 
 echo Copying files from %sourceFolder% to %destinationFolder%...
 
@@ -16,6 +12,6 @@ xcopy /E /I /Y "%sourceFolder%" "%destinationFolder%"
 echo Copy completed.
 
 echo Starting Flow Launcher...
-start "" "C:\Users\xvanhunen\AppData\Local\FlowLauncher\app-1.16.2\Flow.Launcher.exe"
+start "" "C:\Users\jonas\AppData\Local\FlowLauncher\app-1.16.2\Flow.Launcher.exe"
 
 pause
